40c23743ee9250493cf3003efcd69d309b052ca2
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-22 20:11:50 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let name = "nnnnnn";
-name = "rrr";
-console.log(name);
 
 let followers = 100;
 followers = 110;
@@ -16,10 +13,10 @@ console.log(country);
   let age = 10;
   console.log(age);
 }
-// array with const
-// const colors = ["red", "blue", "yellow"];
-// colors.splice(0,1);
-// console.log(colors);
+
+const colors = ["red", "blue", "yellow"];
+colors.splice(0,1);
+console.log(colors);
 
 let nums = [1,2,3,4,5];
 nums.splice(5,0,6);
@@ -31,7 +28,29 @@ let fruits = ["Apple", "Mango"];
 fruits.splice(1,0,"Orange");
 
 console.log(fruits);
+let name = "nihala";
+let age = 18;
+let city = "palakkad";
+let course = "JavaScript";
+console.log(`My name is ${name} and I am ${age} years old .Currently I learning ${course}. i am from ${city}`);
 
-let colors = [ "red", "blue"];
-colors.splice(2,0,"yelow");
-console.log(colors);
+let a = 10;
+let b = 20;
+console.log(`sum =${a + b}`);
+let c = 89;
+let d = 90;
+console.log(`multiply = ${c * d}`);
+
+let price = 10;
+let quantity = 3;
+console.log(`total ${10 * 3}`);
+
+
+let vegetables = ["onion", "carrot", "potato"];
+let [vegetable1,vegetable2,vegetable3] = vegetables
+console.log(vegetable1);
+console.log(vegetable2);
+console.log(vegetable3);
+
+
+
