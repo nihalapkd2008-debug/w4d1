938ee76db075b0db88ad748d597c13b36f082cdf
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-22 11:49:24 +0530

    updated

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ followers = 130;
 console.log(followers);
 
 
+const country = "china";
+
+console.log(country);
+
+{
+  let age = 10;
+  console.log(age);
+}
+// array with const
+// const colors = ["red", "blue", "yellow"];
+// colors.splice(0,1);
+// console.log(colors);
+
+let nums = [1,2,3,4,5];
+nums.splice(5,0,6);
+console.log(nums);
+
+
+let fruits = ["Apple", "Mango"];
+
+fruits.splice(1,0,"Orange");
+
+console.log(fruits);
+
+let colors = [ "red", "blue"];
+colors.splice(2,0,"yelow");
+console.log(colors);
